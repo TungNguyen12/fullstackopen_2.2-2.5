339114fee0c312b2fc590e481f8533bc0c1cefe7
TungNguyen12 <nvtung382@gamil.com>
2023-06-15 14:09:34 +0300

    Reupload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Note from "./components/Note";
 
-function App() {
+import "./App.css";
+
+const Course = ({course}) => {
+  console.log(course.parts.map(part => part.id));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>{course.name}</h1>
+      {course.parts.map(part => <li key={part.id}>{part.name}, {part.exercises} exercises</li> )}
+    </>
+  )
 }
 
-export default App;
+const App = () => {
+
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
