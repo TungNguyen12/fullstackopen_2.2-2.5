c2d0cac4a0a778dcbfe4f29d3d57d86eb14ec6cd
TungNguyen12 <nvtung382@gamil.com>
2023-06-16 10:27:01 +0300

    Upload to branch 2.2

--- src/App.js
+++ src/App.js
@@ -1,58 +1,6 @@
 import React from "react";
 import { useState } from "react";
 import Note from "./components/Note";
-//dasfsdfsdaf
-
-import "./App.css";
-
-// const App = () => {
-//     const courses = [
-//         {
-//             name: "Half Stack application development",
-//             id: 1,
-//             parts: [
-//                 {
-//                     name: "Fundamentals of React",
-//                     exercises: 10,
-//                     id: 1,
-//                 },
-//                 {
-//                     name: "Using props to pass data",
-//                     exercises: 7,
-//                     id: 2,
-//                 },
-//                 {
-//                     name: "State of a component",
-//                     exercises: 14,
-//                     id: 3,
-//                 },
-//                 {
-//                     name: "Redux",
-//                     exercises: 11,
-//                     id: 4,
-//                 },
-//             ],
-//         },
-//         {
-//             name: "Node.js",
-//             id: 2,
-//             parts: [
-//                 {
-//                     name: "Routing",
-//                     exercises: 3,
-//                     id: 1,
-//                 },
-//                 {
-//                     name: "Middlewares",
-//                     exercises: 7,
-//                     id: 2,
-//                 },
-//             ],
-//         },
-//     ];
-
-//     return <Courses courses={courses} />;
-// };
 
 const App = (props) => {
     const [notes, setNotes] = useState(props.notes);
